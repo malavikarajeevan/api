9906ad66a6795899c286d78d66a180f46f5da4c2
malavika <malavikarajeevan85@gmail.com>
2024-01-25 09:08:13 +0530

    new file added

--- spotyfiproj/spotyfiapp/urls.py
+++ spotyfiproj/spotyfiapp/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.Home,name='home'),
+    path('about/',views.about,name='about'),
+    
 ]
--- spotyfiproj/spotyfiapp/views.py
+++ spotyfiproj/spotyfiapp/views.py
@@ -3,9 +3,14 @@ import requests
 
 # Create your views here.
 def Home(request):
-    urls='https://jsonplaceholder.typicode.com/posts'
+    urls='https://dummyjson.com/products'
     res=requests.get(urls)
     if res.status_code==200:
         data=res.json()
         print(data)
     return render(request,'index.html',{'data':data})
+
+
+def about(request):
+
+    return render(request,'about.html')
